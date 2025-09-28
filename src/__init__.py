5d0f7b881654793681f8dea290a8f990a78c4177
dreadew <p74ur@yandex.ru>
2025-09-28 12:04:46 +0300

    fix: poetry build

--- src/__init__.py
+++ src/__init__.py
@@ -9,9 +9,5 @@ __version__ = "0.1.0"
 __author__ = "dreadew"
 __email__ = "p74ur@yandex.ru"
 
-from src.api.server import main as run_server
-from src.application.tools.trino_tools import register_tools
-from src.infra.connection_manager import connection_manager
-from src.core.ddl_analyzer import ddl_analyzer
 
-__all__ = ["run_server", "register_tools", "connection_manager", "ddl_analyzer"]
+__all__ = ["__version__"]
